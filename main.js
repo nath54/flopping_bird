c51a5fcb688ac4dd6bc3c2b767a7c6a255060f02
nath54 <cerisara.nathan@gmail.com>
2019-11-17 11:26:37 +0100

    a

--- main.js
+++ main.js
@@ -177,5 +177,5 @@ ctx.fillStyle="rgb(200,200,0)";
 ctx.fillText("Appuyez sur", 50 ,200);
 ctx.fillText("le bouton (jouer)", 50 ,250);
 ctx.fillText("pour jouer !", 50 ,300);
-
+ctx.drawImage(bide.imgs[0], 100, 500, 100, 100)
 
