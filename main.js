1a9a7e9f5575fd50600c5999d96e910160b405fa
nath54 <cerisara.nathan@gmail.com>
2019-11-17 11:46:56 +0100

    a

--- main.js
+++ main.js
@@ -177,6 +177,7 @@ ctx.fillStyle="rgb(200,200,0)";
 ctx.fillText("Appuyez sur", 50 ,200);
 ctx.fillText("le bouton (jouer)", 50 ,250);
 ctx.fillText("pour jouer !", 50 ,300);
-ctx.drawImage(bide.imgs[bide.imgs.length-1], 100, 500, 100, 100)
-ctx.fillText("Attendez juste qu'un oiseau apparaisse à l'écran", 10 ,600);
+ctx.drawImage(bide.imgs[0], 100, 500, 100, 100)
+ctx.fillText("Attendez juste qu'un oiseau", 10 ,600);
+ctx.fillText("apparaisse à l'écran", 10 ,630);
 
